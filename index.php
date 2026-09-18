b2a0b3e5b9d7cc249359937bacef724625b83bf3
PlaTypus06 <ghiffarithoriq@gmail.com>
2026-09-18 11:58:08 +0800

    update menu home

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
 require_once "config/database.php";
+require_once "helpers/keamanan.php";
 
-echo "Koneksi database berhasil!";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+    <h1>Selamat Datang</h1>
+    <p>Silakan pilih menu:</p>
+
+    <a href="customers.php">
+        <button>Data Customer</button>
+    </a>
+    <a href="pesanan.php">
+        <button>Data Pesanan</button>
+    </a>
+
+</body>
+</html>
